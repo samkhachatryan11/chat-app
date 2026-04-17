e798782e9c13689097d0bafc4474debcc212d08a
Sam Khachatryan <khachatryansam003@gmail.com>
2026-04-17 20:21:29 +0400

    Added guest only routes & improved the protection for routes

--- frontend/src/app/router/AuthRoutes.tsx
+++ frontend/src/app/router/AuthRoutes.tsx
@@ -1,17 +1,23 @@
 import { LoginPage, RegisterPage } from "@/features/auth/pages";
 import { AuthLayout } from "@/app/layouts/AuthLayout";
+import { GuestOnlyRoute } from "./guestOnlyRoute";
 
 export default {
-  name: "Auth",
-  element: <AuthLayout />,
+  element: <GuestOnlyRoute />,
   children: [
     {
-      path: "/login",
-      element: <LoginPage />,
-    },
-    {
-      path: "/register",
-      element: <RegisterPage />,
+      name: "Auth",
+      element: <AuthLayout />,
+      children: [
+        {
+          path: "/login",
+          element: <LoginPage />,
+        },
+        {
+          path: "/register",
+          element: <RegisterPage />,
+        },
+      ],
     },
   ],
 };
--- frontend/src/app/router/GuestOnlyRoute.tsx
+++ frontend/src/app/router/GuestOnlyRoute.tsx
@@ -1,8 +1,8 @@
 import { useAuthStore } from "@/features/auth/stores/useAuthStore";
 import { Navigate, Outlet } from "react-router";
 
-export function ProtectedRoute() {
+export function GuestOnlyRoute() {
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated());
 
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
+  return isAuthenticated ? <Navigate to="/" replace /> : <Outlet />;
 }
